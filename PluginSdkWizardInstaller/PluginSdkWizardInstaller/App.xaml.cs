38bc9a5d705e47350622c46ae96888ff6db8a318
DK22Pac <gdamdk@gmail.com>
2017-10-20 04:12:25 +0300

    Plugin-SDK Installer interface

---
+++ PluginSdkWizardInstaller/PluginSdkWizardInstaller/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PluginSdkWizardInstaller {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
