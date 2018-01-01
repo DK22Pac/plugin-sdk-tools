fabbcf40e9ebbbb3e55c142b8446ed1fcfa51022
DK22Pac <gdamdk@gmail.com>
2018-01-01 22:21:45 +0200

    update installer-launcher (admin rights)

--- PluginSdkInstallerLauncher/PluginSdkInstallerLauncher.cpp
+++ PluginSdkInstallerLauncher/PluginSdkInstallerLauncher.cpp
@@ -6,7 +6,7 @@ using namespace std::experimental::filesystem;
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
     path installerPath = current_path() / "tools" / "installer" / "PluginSdkWizardInstaller.exe";
-    if (!ShellExecuteA(NULL, NULL, installerPath.string().c_str(), NULL, NULL, SW_SHOWNORMAL)) {
+    if (!ShellExecuteA(NULL, "runas", installerPath.string().c_str(), NULL, NULL, SW_SHOWNORMAL)) {
         std::string errMessage = "Unable to launch '" + installerPath.string() + "'";
         MessageBox(NULL, errMessage.c_str(), "Plugin-SDK Installer Launcher - Error", 0);
     }
